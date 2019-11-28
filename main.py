57dd61dfea87304c0f18f8582a91ccef5f5cd0c0
Nick <lis@chaikovskie.com>
2019-11-28 05:35:03 +0300

    Some code improvements, shortened code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from PyQt5 import uic
 from PyQt5.Qt import QMainWindow, QApplication
 from pptx import Presentation
-from pptx.enum.shapes import PP_PLACEHOLDER_TYPE, MSO_SHAPE_TYPE
+from pptx.enum.shapes import MSO_SHAPE_TYPE
 
 file_log = open('log.txt', 'w', encoding="utf-8")
 
@@ -13,152 +13,72 @@ def get_file_content(files):
     for file in files:
         slide_counter = 0  # Счётчик слайдов
         presentation = Presentation(f'{file}')
-        presentation_slides_count = len(presentation.slides)  # Получаем общее количество слайдов
+        presentation_slides_number = len(presentation.slides)  # Получаем общее количество слайдов
 
-        res = {'Первый слайд': [], 'Второй слайд': [], 'Третий слайд': [], 'Другие слайды': []}
+        res = {'1 слайд': [], '2 слайд': [], '3 слайд': [], 'Другие слайды': []}
+        possible_shapes_placeholders = [
+            "PP_PLACEHOLDER_TYPE.CENTER_TITLE",
+            "PP_PLACEHOLDER_TYPE.SUBTITLE",
+            "PP_PLACEHOLDER_TYPE.TITLE",
+            "PP_PLACEHOLDER_TYPE.BODY",
+            "PP_PLACEHOLDER_TYPE.OBJECT",
+            "PP_PLACEHOLDER_TYPE.PICTURE",
+        ]
+        possible_shapes_mso = [
+            "MSO_SHAPE_TYPE.PICTURE",
+            "MSO_SHAPE_TYPE.AUTO_SHAPE",
+            "MSO_SHAPE_TYPE.TEXT_BOX",
+        ]
         file_log.write(f'Файл: {file}\n')
         for slide in presentation.slides:
             slide_counter += 1
             file_log.write(f'Начало {slide_counter} слайда:\n')
             for shape in slide.shapes:
-                if slide_counter == 1:
-                    if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
-                        if shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.CENTER_TITLE:
-                            res['Первый слайд'].append('Найден главный заголовок')
-                            file_log.write(f'Найден главный заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.SUBTITLE:
-                            res['Первый слайд'].append('Найден подзаголовок')
-                            file_log.write(f'Найден подзаголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.TITLE:
-                            res['Первый слайд'].append('Найден заголовок')
-                            file_log.write(f'Найден заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.BODY:
-                            res['Первый слайд'].append('Найден body')
-                            file_log.write(f'Найден body:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.OBJECT:
-                            res['Первый слайд'].append('Найден обьект')
-                            file_log.write(f'Найден обьект:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.PICTURE:
-                            res['Первый слайд'].append('Найдена картинка')
-                            file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                        res['Первый слайд'].append('Найдена картинка')
-                        file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.AUTO_SHAPE:
-                        res['Первый слайд'].append('Найден auto_shape')
-                        file_log.write(f'Найден auto_shape:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
-                        res['Первый слайд'].append('Найден text_box')
-                        file_log.write(f'Найден text_box:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                    else:
-                        print('Uncaught error')
-                        break
-                elif slide_counter == 2:
-                    if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
-                        if shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.CENTER_TITLE:
-                            res['Второй слайд'].append('Найден главный заголовок')
-                            file_log.write(f'Найден главный заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.SUBTITLE:
-                            res['Второй слайд'].append('Найден подзаголовок')
-                            file_log.write(f'Найден подзаголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.TITLE:
-                            res['Второй слайд'].append('Найден заголовок')
-                            file_log.write(f'Найден заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.BODY:
-                            res['Второй слайд'].append('Найден body')
-                            file_log.write(f'Найден body:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.OBJECT:
-                            res['Второй слайд'].append('Найден обьект')
-                            file_log.write(f'Найден обьект:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.PICTURE:
-                            res['Второй слайд'].append('Найдена картинка')
-                            file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                        res['Второй слайд'].append('Найдена картинка')
-                        file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.AUTO_SHAPE:
-                        res['Второй слайд'].append('Найден auto_shape')
-                        file_log.write(f'Найден auto_shape:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
-                        res['Второй слайд'].append('Найден text_box')
-                        file_log.write(f'Найден text_box:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                    else:
-                        print('Uncaught error')
-                        break
-                elif slide_counter == 3:
-                    if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
-                        if shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.CENTER_TITLE:
-                            res['Третий слайд'].append('Найден главный заголовок')
-                            file_log.write(f'Найден главный заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.SUBTITLE:
-                            res['Третий слайд'].append('Найден подзаголовок')
-                            file_log.write(f'Найден подзаголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.TITLE:
-                            res['Третий слайд'].append('Найден заголовок')
-                            file_log.write(f'Найден заголовок:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.BODY:
-                            res['Третий слайд'].append('Найден body')
-                            file_log.write(f'Найден body:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.OBJECT:
-                            res['Третий слайд'].append('Найден обьект')
-                            file_log.write(f'Найден обьект:\n')
-                            if hasattr(shape, "text"):
-                                file_log.write(f'{shape.text}\n')
-                        elif shape.placeholder_format.type == PP_PLACEHOLDER_TYPE.PICTURE:
-                            res['Третий слайд'].append('Найдена картинка')
-                            file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                        res['Третий слайд'].append('Найдена картинка')
-                        file_log.write(f'Найдена картинка.\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.AUTO_SHAPE:
-                        res['Третий слайд'].append('Найден auto_shape')
-                        file_log.write(f'Найден auto_shape:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                    elif shape.shape_type == MSO_SHAPE_TYPE.TEXT_BOX:
-                        res['Третий слайд'].append('Найден text_box')
-                        file_log.write(f'Найден text_box:\n')
-                        if hasattr(shape, "text"):
-                            file_log.write(f'{shape.text}\n')
-                else:
-                    res['Другие слайды'].append('Найдены.')
-                    file_log.write(f'Найдены слайды больше 4го.\n')
+                for number in range(1, presentation_slides_number + 1):
+                    if slide_counter == number and number < 4:
+                        if shape.shape_type == MSO_SHAPE_TYPE.PLACEHOLDER:
+                            for possible_shape_placeholder in possible_shapes_placeholders:
+                                if shape.placeholder_format.type is eval(possible_shape_placeholder):
+                                    res[f'{number} слайд'].append(f' Найден объект {possible_shape_placeholder} \n')
+                                    file_log.write(f' Найден обьект {possible_shape_placeholder} \n')
+                                    if hasattr(shape, "text"):
+                                        file_log.write(f'  Текст на объекте: {shape.text} \n')
+                                        if hasattr(shape, "text_frame"):
+                                            font_sizes = []
+                                            if font_sizes:
+                                                font_sizes.clear()
+                                            for paragraph in shape.text_frame.paragraphs:
+                                                for run in paragraph.runs:
+                                                    try:
+                                                        font_sizes.append(run.font.size.pt)
+                                                    except AttributeError:
+                                                        pass
+                                            else:
+                                                if font_sizes:
+                                                    file_log.write(f'   Размер текста {max(font_sizes)} \n')
+                        else:
+                            for possible_shape_mso in possible_shapes_mso:
+                                if shape.shape_type is eval(possible_shape_mso):
+                                    res[f'{number} слайд'].append(f' Найден объект {possible_shape_mso} \n')
+                                    file_log.write(f' Найден обьект {possible_shape_mso} \n')
+                                    if hasattr(shape, "text"):
+                                        file_log.write(f'  Текст на объекте: {shape.text} \n')
+                                        if hasattr(shape, "text_frame"):
+                                            font_sizes = []
+                                            if font_sizes:
+                                                font_sizes.clear()
+                                            for paragraph in shape.text_frame.paragraphs:
+                                                for run in paragraph.runs:
+                                                    try:
+                                                        font_sizes.append(run.font.size.pt)
+                                                    except AttributeError:
+                                                        pass
+                                            else:
+                                                if font_sizes:
+                                                    file_log.write(f'   Размер текста {max(font_sizes)} \n')
+                    elif number >= 4:
+                        res['Другие слайды'].append('Найдены.')
+                        file_log.write(f'Найдены слайды больше 4го.\n')
 
         for result in res:
             print(f'{result} -> {res.get(result)}')
