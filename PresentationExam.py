4edb69ae8d12f6c01407f0f83800e2a0aa91fabb
newfox79 <znaykaznaykovich@gmail.com>
2020-03-15 19:56:26 +0300

    chore: minor changes, added images instance to Analyze, removed delete temp functionality

--- PresentationExam.py
+++ PresentationExam.py
@@ -20,11 +20,11 @@ class PresentationExam(object):
     def __init__(self, path_to_presentation):
         super().__init__()
         self._path = path_to_presentation
-        self._Application = win32com.client.gencache.EnsureDispatch("PowerPoint.Application")
+        self._Application = win32com.client.Dispatch("PowerPoint.Application")
         self._Presentation = self._Application.Presentations.Open(self._path, WithWindow=False)
         self._Utils = Utils(self._Application)
         self._Images = Images(self._Presentation, self._Utils)
-        self._Analyze = Analyze(self._Presentation, self._Application, self._Utils)
+        self._Analyze = Analyze(self._Presentation, self._Application, self._Utils, self._Images)
 
     @property
     def Presentation(self):
@@ -42,15 +42,7 @@ class PresentationExam(object):
         return self._Presentation.Save()
 
     def __del__(self):
-        if os.path.isdir(os.getcwd() + "\\temp\\"):
-            shutil.rmtree(os.getcwd() + "\\temp\\")
         self._Application.Quit()
 
     def __exit__(self):
-        if os.path.isdir(os.getcwd() + "\\temp\\"):
-            shutil.rmtree(os.getcwd() + "\\temp\\")
         self._Application.Quit()
-
-
-PresentationExam = PresentationExam(r"D:\Presentations\002.pptx")
-print(PresentationExam.Images.get("skeleton"))
