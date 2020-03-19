21515c2821a0f5ff18688e28cb3433f080e0d925
newfox79 <znaykaznaykovich@gmail.com>
2020-03-19 19:11:42 +0300

    fix(compare): compare now compares uploaded files with original files extracted from zip pptx

--- PresentationExamImages.py
+++ PresentationExamImages.py
@@ -1,6 +1,7 @@
 import inspect
 import os
 import shutil
+import zipfile
 
 import cv2
 import imagehash
@@ -29,16 +30,16 @@ class PresentationExamImages(object):
             outline=outline
         )
 
-    def __generate_images_skeleton(self, return_path=False, return_bool=False):
-        if not os.path.exists('temp'):
-            os.mkdir('temp')
+    def __generate_images_skeleton(self, return_path=False, return_bool=False, directory="temp"):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
         for Slide in self._Presentation.Slides:
             skeleton = Image.new("RGB",
                                  color="#ffffff",
                                  size=(self._Utils.convert_points_px(self._Presentation.PageSetup.SlideWidth),
                                        self._Utils.convert_points_px(self._Presentation.PageSetup.SlideHeight))
                                  )
-            skeleton_path = os.path.abspath(f"temp/skeleton_{Slide.SlideIndex}.jpg")
+            skeleton_path = os.path.abspath(f"{directory}/skeleton_{Slide.SlideIndex}.jpg")
             skeleton_draw = ImageDraw.Draw(skeleton)
             for Shape in Slide.Shapes:
                 shape_dimensions = self._Utils.get_shape_dimensions(Shape)
@@ -56,19 +57,19 @@ class PresentationExamImages(object):
         if return_bool is True:
             return True
 
-    def __generate_images_layout(self, lt="layout_1"):
+    def __generate_images_layout(self, lt="layout_1", directory="temp"):
         """
         Experimental
         :return: None
         """
-        if not os.path.exists('temp'):
-            os.mkdir('temp')
+        if not os.path.exists(directory):
+            os.mkdir(directory)
         width = self._Utils.convert_points_px(self._Presentation.PageSetup.SlideWidth)
         height = self._Utils.convert_points_px(self._Presentation.PageSetup.SlideHeight)
         result = []
         layout = self._layouts[lt][1](width, height)
         for slide in layout:
-            image_path = os.getcwd() + "\\temp\\" + f"image_{slide}.png"
+            image_path = os.path.abspath(f"{directory}/image_{slide}.png")
             cv2.imwrite(image_path, 255 * np.ones((height, width, 3), np.uint8))
             image = cv2.imread(image_path)
             overlay = image.copy()
@@ -77,13 +78,16 @@ class PresentationExamImages(object):
                 cv2.rectangle(overlay, (l, t), (w + l, h + t), (0, 255, 0), -1)
             for t in layout[slide]['text_blocks']:
                 l, t, w, h = int(t[0]), int(t[1]), int(t[2]), int(t[3])
-                cv2.rectangle(overlay, (l, t), (w+l, h+t), (255, 0, 0), -1)
+                cv2.rectangle(overlay, (l, t), (w + l, h + t), (255, 0, 0), -1)
             cv2.addWeighted(overlay, 0.7, image, 0.3, 0, image)
             cv2.imwrite(image_path, image)
             result.append(image_path)
         return result
 
-    def __get_original_images_from_presentation(self, return_path=False, return_bool=False):
+    def __get_picture_shape_images(self, return_path=False, return_bool=False):
+        """
+        Reserved for internal use
+        """
         if not os.path.exists('shapes_images'):
             os.mkdir('shapes_images')
         counter = 0
@@ -98,34 +102,38 @@ class PresentationExamImages(object):
         if return_bool:
             return True
 
-    def __compare_images(self):
-        path_to_shape_images = list(self.__get_original_images_from_presentation(return_path=True))
+    def __save_original_images_presentation(self, directory="original_images_presentation"):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        file, c = zipfile.ZipFile(os.path.join(self._Presentation.Path, self._Presentation.Name)), 0
+        for f in file.namelist():
+            if f.startswith('ppt/media'):
+                save_path = os.path.abspath(f"{directory}/image_{c}.jpg")
+                shutil.copyfileobj(file.open(f), open(save_path, "wb"))
+                c += 1
+                yield save_path
+
+    def __compare_images(self, directory="original_images"):
+        path_to_shape_images = list(self.__save_original_images_presentation())
         compare_images_counter = 0
-        for original_image_name in os.listdir(os.getcwd() + "\\original_images\\"):
+        for original_image_name in os.listdir(os.path.abspath(directory)):
             for shape_image_path in path_to_shape_images:
-                original_image = Image.open(os.getcwd() + "\\original_images\\" + original_image_name)
+                original_image = Image.open(os.path.abspath(f"{directory}/{original_image_name}"))
                 shape_image = Image.open(shape_image_path)
+                print(imagehash.average_hash(original_image), imagehash.average_hash(shape_image))
                 if imagehash.average_hash(original_image) == imagehash.average_hash(shape_image):
                     compare_images_counter += 1
-                    path_to_shape_images.remove(shape_image_path)
-                    break
-                elif imagehash.average_hash(shape_image) - imagehash.average_hash(original_image) <= 10:
-                    compare_images_counter += 1
-                    path_to_shape_images.remove(shape_image_path)
                     break
         else:
-            #print(path_to_shape_images)
             if len(path_to_shape_images) <= 2:
-                shutil.rmtree(os.getcwd() + "\\shapes_images\\")
                 return True
-            shutil.rmtree(os.getcwd() + "\\shapes_images\\")
             return False
 
-    def __generate_images_screenshots(self, return_path=False, return_bool=False):
-        if not os.path.exists('temp'):
-            os.mkdir('temp')
+    def __generate_images_screenshots(self, return_path=False, return_bool=False, directory="temp"):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
         for Slide in self._Presentation.Slides:
-            screenshot_path = os.path.abspath(f"temp/slide_{Slide.SlideIndex}.jpg")
+            screenshot_path = os.path.abspath(f"{directory}/slide_{Slide.SlideIndex}.jpg")
             Slide.Export(screenshot_path, "JPG")
             if return_path is True:
                 yield screenshot_path
@@ -148,8 +156,8 @@ class PresentationExamImages(object):
                         return True
         return False
 
-    def compare(self):
-        if os.path.exists(os.getcwd() + "\\original_images\\"):
+    def compare(self, directory="original_images"):
+        if os.path.exists(os.path.abspath(directory)):
             return self.__compare_images()
         else:
             return "Не загружены изображения."
