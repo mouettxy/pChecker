4ec53aabb04857376fb2a90cd799eb047ad03769
newfox79 <znaykaznaykovich@gmail.com>
2020-04-01 02:36:17 +0300

    feature: added presentation open function, moved upload files from Images class, removed open and save methods from PresentationExam, added Utils property

--- PresentationExam.py
+++ PresentationExam.py
@@ -1,17 +1,30 @@
 # coding: utf-8
+import shutil
+from pathlib import Path
+
 import win32com.client
 
 from PresentationExamAnalyze import PresentationExamAnalyze as Analyze
 from PresentationExamImages import PresentationExamImages as Images
 from PresentationExamUtils import PresentationExamUtils as Utils
 
-'''
-CHEATSHEET
-font_size - Shape.TextFrame.TextRange.Font.Size
-font_name - Shape.TextFrame.TextRange.Font.Name
-first_line - Shape.TextFrame.TextRange.Lines(1, 1)
-id - Shape.Id
-'''
+
+def open_presentation(path):
+    win32com.client.Dispatch("PowerPoint.Application").Presentations.Open(path)
+
+
+def upload_images(from_directory):
+    f_dir = Path(from_directory).resolve()
+    if f_dir.is_dir():
+        path = Path('original_images').resolve()
+        if path.exists():
+            shutil.rmtree(path, ignore_errors=True)
+        path.mkdir(parents=True)
+        for filename in f_dir.iterdir():
+            if filename.suffix in ['.png', '.jpg', '.jpeg']:
+                shutil.copy(filename, path, follow_symlinks=True)
+        return True
+    return False  # TODO generate expression here
 
 
 class PresentationExam(object):
@@ -25,6 +38,10 @@ class PresentationExam(object):
         self._Analyze = Analyze(self._Presentation, self._Application, self._Utils, self._Images)
 
     @property
+    def Utils(self):
+        return self._Presentation
+
+    @property
     def Presentation(self):
         return self._Presentation
 
@@ -35,12 +52,6 @@ class PresentationExam(object):
     @property
     def Analyze(self):
         return self._Analyze
-
-    def open(self) -> None:
-        self._Presentation.NewWindow().Activate()
-
-    def save(self):
-        return self._Presentation.Save()
 
     def __del__(self):
         self._Application.Quit()
